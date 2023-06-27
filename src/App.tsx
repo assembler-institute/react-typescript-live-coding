fd1f0181e7bf7671587ec998a781cc8c7eedf17b
Jose Valenzuela <guadix88@gmail.com>
2023-06-27 16:22:07 +0200

    feat(api with .env): added a json-server, and the script. Also added a .env where to store valuable info

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import './App.css'
-import Products from './components/Products/Products'
-
+import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
-
-  return (
-    <>
-      <h2>E-commerce</h2>
-      <Products id={1} title={'iPhone 14'} price={500} counter={0} />
-      <Products id={1} title={'iPhone 15'} price={600} counter={0} />
-    </>
-  )
+	return (
+		<>
+			<ProductList />
+		</>
+	);
 }
 
-export default App
+export default App;
